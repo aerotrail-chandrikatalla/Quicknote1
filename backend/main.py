582220189279c2105be38e3d63b3ecfe42aaf4f0
aerotrail-chandrikatalla <chandrika.talla@aerotrail.in>
2025-08-30 17:40:18 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -5,9 +5,7 @@ from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# -------------------------
-# FastAPI app setup
-# -------------------------
+
 app = FastAPI()
 
 app.add_middleware(
@@ -24,11 +22,13 @@ engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
+# Table model
 class NoteDB(Base):
     __tablename__ = "notes"
     id = Column(Integer, primary_key=True, index=True)
     subject = Column(String, index=True)
     description = Column(String, index=True)
+
 
 Base.metadata.create_all(bind=engine)
 
